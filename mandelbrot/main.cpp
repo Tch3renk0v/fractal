acf04a0621fa281d41255dae6e416936ba989ae4
Tcherenk0v <thecelte13@gmail.com>
2019-01-17 08:44:58 +0100

    Initial commit

---
+++ mandelbrot/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 17 janv. 2019
+ *      Author: tcherenkov
+ */
+
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+
+
+
+
+
+  std::cout<< "Done !" << std::endl;
+  return 0;
+}
+
